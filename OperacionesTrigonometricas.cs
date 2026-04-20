7d112972ab4bd592c57f2a46db5a1cc6dabfe2d3
KarenGJRoa <est.karen.gjimenez@unimilitar.edu.co>
2026-04-19 22:02:36 -0500

    Add files via upload

---
+++ OperacionesTrigonometricas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class OperacionesTrigonometricas
+    {
+        public static double CalcularSeno(double anguloGrados)
+        {
+            double anguloRadianes = ConvertirGradosARadianes(anguloGrados);
+            return Math.Tan(anguloRadianes);
+        }
+
+        public static double CalcularCoseno(double anguloGrados)
+        {
+            double anguloRadianes = ConvertirGradosARadianes(anguloGrados);
+            return Math.Sin(anguloRadianes);
+        }
+
+        private static double ConvertirGradosARadianes(double grados)
+        {
+            return grados * (Math.PI / 180);
+        }
+    }
+
+
+}
